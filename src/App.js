3957cb6a09f55c2a4e314802d073c6e046e998c4
bvp1518 <bvp18061992@gmail.com>
2024-08-14 13:18:22 +0530

    till-254. Getting Values via FormData & Native Browser APIs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
-import Login from "./components/Login";
+import Signup from "./components/SignUp";
+
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
